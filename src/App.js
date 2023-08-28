39f849f8961984c9b9be5a928415b2b171b93ff9
krishna <krishnadj7431@gmail.com>
2023-08-28 13:07:41 +0530

    modified Site navbar updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
 import Navbar from './navbar';
+import Navbar2 from './navbar2';
 import AllVideos from './components/videos/videos/AllVideos';
 
 function App() {
   return (
     <div className="App">
-        <Navbar />
+        <Navbar2 />
         <AllVideos />
         
     </div>
